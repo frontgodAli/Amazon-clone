5cde27017cf7ccba9f9125a349515d576e6da744
Ali hassi <alihassiperso@gmail.com>
2023-12-24 05:04:02 +0100

    cart selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -66,12 +66,15 @@ document.querySelectorAll(".add-to-cart-button").forEach((button)=>{
             matchingTest=product;
         }
     });
+    const selectQuantity=document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity=Number(selectQuantity.value);
+
         if(matchingTest){
-            matchingTest.quantity+=1;
+            matchingTest.quantity+=quantity;
         }else{
             cart.push({
             productId:productId,
-            quantity:1
+            quantity:quantity
         });
         }
 
